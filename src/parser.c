16f2ec90637e9ddd0fc1920917e822673d321909
Anna Korunska <akorunska@gmail.com>
2018-11-27 23:44:27 +0200

    parsing width of the map

--- src/parser.c
+++ src/parser.c
@@ -12,13 +12,30 @@
 
 #include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include "libft.h"
 #include "fdf.h"
+
+int	count_digits_per_row(char *row)
+{
+	char **digits;
+	int count;
+
+	digits = ft_strsplit(row, ' ');
+	count = 0;
+	while (digits[count] != NULL)
+	{
+		free(digits[count]);
+		count++;
+	}
+	free(digits);
+	return (count);
+}
 
 int fill_map_metrics(t_map *m, char *file)
 {
 	int		fd;
-	// int		current_w;
+	int		current_w;
 	char	*row;
 
 	fd = open(file, O_RDONLY);
@@ -27,10 +44,16 @@ int fill_map_metrics(t_map *m, char *file)
 		m->h = 0;
 		while (get_next_line(fd, &row))
 		{
-			(m->h)++;
+			current_w = count_digits_per_row(row);
 			free(row);
+			if (!(m->h))
+				(m->w) = current_w;
+			if (m->w != current_w)
+				return (0);
+			(m->h)++;
 		}
 		close(fd);
+		return (1);
 	}
 	return (0);
 }
@@ -43,5 +66,6 @@ t_map *parse_map(char *file)
 	if (map == NULL)
 		return NULL;
 	fill_map_metrics(map, file);
+	ft_printf("the map has metrics: w -- %d; h -- %d", map->w, map->h);
 	return map;
 }
